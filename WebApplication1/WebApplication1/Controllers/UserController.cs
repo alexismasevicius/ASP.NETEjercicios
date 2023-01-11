d97bc7f7a71831e48ad23907491e6f1ebccc8646
alexis m <alexismasevicius@gmail.com>
2023-01-11 16:00:33 -0300

    cambios de ayer

---
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.TableViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            List<UserTableViewModel> lst = null;
+            using (cursomvcEntities db = new cursomvcEntities())
+            {
+                lst = (from d in db.users
+                       where d.idState == 1
+                       orderby d.email
+                       select new UserTableViewModel
+                       {
+                           Email = d.email,
+                           Id = d.id,
+                           Edad = d.edad
+                       }).ToList();
+            }
+
+            return View(lst);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/ViewModel/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.ViewModel
+{
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo Electronico")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Cofirma conntraseña")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Edad { get; set; }
+
+
+
+    }
+}
